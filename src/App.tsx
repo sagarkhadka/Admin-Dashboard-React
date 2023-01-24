6aa6add94df5e23a72fd750051668b827a5d5229
sagarkhadka <khadkasagar109@gmail.com>
2023-01-24 18:46:00 +0545

    Here, we go

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Sidemenu from "./components/Sidemenu"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex gap-10">
+
+        <Sidemenu />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quibusdam quisquam sapiente quasi non placeat laudantium magni possimus, aperiam perspiciatis culpa eligendi earum temporibus totam voluptatem cum nesciunt.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidemenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidemenu = () => {
+  return (
+    <>
+      <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+      </ul>
+    </>
+  )
+}
+
+export default Sidemenu
